9146f341ff6e31cd6aa4029b0a7cf3f548a12594
Mohammad Daud Ibrahim <daud.ibrahim@mds.com.bd>
2024-01-31 08:10:17 +0600

    Revert "find_sum_of_three"
    This reverts commit a569d8f40e59646bc2ad4d751c777ea44da55979.

--- src/python_concepts/repl.py
+++ src/python_concepts/repl.py
@@ -1,57 +1,32 @@
-"""
-The 3Sum problem requires examining all possible combinations
-of three elements in the array,and this involves nested iterations.
-Therefore, the time complexity of any algorithm solving the 3Sum problem
-is lower-bounded by O(n^2), where "n" is the length of the array.
-"""
+class Solution:
+    def isPalindrome(self, inputString: str) -> bool:
+        start = 0
+        end = len(inputString) - 1
+
+        while start < end:
+            while inputString[start].isalnum() is False:
+                start += 1
+                if start > len(inputString) - 1:
+                    return True
+
+            while inputString[end].isalnum() is False:
+                end -= 1
+                if end < 0:
+                    return True
+
+            if inputString[start].lower() != inputString[end].lower():
+                return False
+
+            start += 1
+            end -= 1
+
+        return True
 
 
-def find_sum_of_three(nums, target):
-    low = 0
-    high = len(nums) - 1
-    pivot = low + 1
+# create an instance of the Solution class
+sol = Solution()
 
-    nums.sort()
-    print(nums)
+# call the isPalindrome method on the instance
+result = sol.isPalindrome("A man, a plan, a canal: Panama")
 
-    while low < high and pivot < high:
-        current_sum = nums[low] + nums[pivot] + nums[high]
-
-        if current_sum == target:
-            return True
-        elif current_sum > target:
-            high -= 1
-        else:
-            # check each combination, spent a lot time figuring this.
-            diff = pivot - low
-            if diff == 1:
-                pivot += 1
-            else:
-                low += 1
-
-    return False
-
-
-def find_sum_of_three_alternate(nums, target):
-    # Sort the input array
-    nums.sort()
-
-    # Iterate through the array
-    for i in range(len(nums) - 2):
-        # Initialize pointers
-        low = i + 1
-        high = len(nums) - 1
-
-        # Adjust pointers based on current sum
-        while low < high:
-            current_sum = nums[i] + nums[low] + nums[high]
-
-            if current_sum == target:
-                return True
-            elif current_sum < target:
-                low += 1
-            else:
-                high -= 1
-
-    # No triplet found
-    return False
+print(result)
